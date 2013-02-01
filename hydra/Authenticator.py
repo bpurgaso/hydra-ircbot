9c9755cc9fb8bb19124950daa807a539bcb63ec3
Benjamin Purgason <nullrouteftw@gmail.com>
2013-02-01 08:36:37 -0800

    Quick update this morning, starting to flesh out the config and Authenticator

--- hydra/Authenticator.py
+++ hydra/Authenticator.py
@@ -25,11 +25,20 @@ class Authenticator(object):
         f.close()
         return tmp
 
+    def sanityCheck(self):
+        pass
+        '''
+        all inherits_from are valid
+        all command entries within a group map to valid commands
+        all group entries within a user map to valid group
+        '''
+
     def saveConfigToDisk(self):
         f = open('authenticate.yaml', 'w')
         f.write(yaml.dump(self.config, default_flow_style=False))
         f.close()
 
+    #utility methods
     def inheritsFrom(self, group):
         return self.config['groups'][group]['inherits_from']
 
@@ -45,12 +54,31 @@ class Authenticator(object):
     def getAvailableCommandsForGroup(self, group):
         commands = self.config['groups'][group]['commands']
 
+        if '*' == commands[0]:
+            return self.getAllCommands()
+
         while True:
             group = self.config['groups'][group]['inherits_from']
             commands.extend(self.config['groups'][group]['commands'])
             if self.config['groups'][group]['inherits_from'] == 'None':
                 break
         return commands
+
+    def getAllCommands(self):
+        return self.config['commands'].keys()
+
+    #convenience methods
+    def isUserAuthorized(self, command, user):
+        if command in self.getAvailableCommandsForUser(user):
+            return True
+        else:
+            return False
+
+    def isGroupAuthorized(self, command, group):
+        if command in self.getAvailableCommandsForUser(group):
+            return True
+        else:
+            return False
 
 ###dummy code (driver / test code)
 at = Authenticator()
