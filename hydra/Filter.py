ec9e3b37775b30a0a32b859856c657ce7a2316ca
Benjamin Purgason <nullrouteftw@gmail.com>
2013-02-03 13:41:01 -0800

    super bowl, will commit the body of hydra soon

--- hydra/Filter.py
+++ hydra/Filter.py
@@ -5,13 +5,27 @@ Created on Feb 3, 2013
 '''
 
 
+'''
+I need to take time to carefully consider how to go about
+implementing this functionality.
+'''
+
+
 class FilterManager(object):
     '''
     classdocs
     '''
 
-    def __init__(self):
+    def __init__(self, configManager):
         '''
         Constructor
         '''
+        self.configManager = configManager
+        self.configManager.registerListener(self)
+        self.config = self.reloadConfig()
+
+    def reloadConfig(self):
+        self.config = self.configManager.getConfig()
+
+    def filterMsg(self, message, user, channel):
         pass
