314ddfb63fe71517cbfee3eb135ade553fde05cd
Benjamin Purgason <nullrouteftw@gmail.com>
2013-02-03 17:29:41 -0800

    49ers are losing...

--- hydra/Hydra.py
+++ hydra/Hydra.py
@@ -14,17 +14,62 @@ from Authenticator import Authenticator
 from Executor import Executor
 
 
+class bot(irc.IRCClient):
+    """
+    irc bots, yay
+    """
+    def _get_nickname(self):
+        return self.factory.nickname
+    nickname = property(_get_nickname)
+
+    '''
+    Fill in the rest...
+    '''
+
+
+class botFactory(protocol.ClientFactory):
+    """
+    Factory for producing "bot"
+    """
+    protocol = bot
+
+    def __init__(self, channel, configManager, auth, executor):
+        self.startChannel = channel
+        self.configManager = configManager
+        self.config = self.configManager.getConfig()
+        self.auth = auth
+        self.executor = executor
+
+        #required
+        self.nickname = self.config['nick']
+
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection (%s), reconnecting." % (reason)
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Could not connect:  %s" % (reason)
+
+
 class Hydra(object):
     '''
     The big bad scary bot
     '''
 
     def __init__(self):
+        self.startChannel = '#hydra'
         self.configManager = ConfigManager()
         self.config = self.configManager.getConfig()
         self.configManager.registerListener(self)
         self.auth = Authenticator(self.configManager)
         self.executor = Executor(self.configManager, self.auth)
+
+        n = self.config['network']
+        p = self.config['port']
+        b = botFactory(self.startChannel, self.configManager, self.auth,\
+                       self.executor)
+        reactor.connectTCP(n, p, b)  # @UndefinedVariable
+        reactor.run()  # @UndefinedVariable
 
     def reloadConfig(self):
         self.config = self.configManager.getConfig()
